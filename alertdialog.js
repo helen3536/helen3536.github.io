60c79750aa70becdae217e0761cda60d774307d6
helen <helen3536@gmail.com>
2017-11-23 11:44:43 +0700

    website

---
+++ alertdialog.js
@@ -0,0 +1,15 @@
+<html>
+<head>
+<script type="text/javascript"> <!--function Warn(){alert ('This is a warning messege!"); //document.write ("This is a warning msg!");}
+function Warn2() {alert ("This is aother warning messege!"); //document.write ("This is another warning msg!");}
+</script>
+</head>
+                  
+<body>
+<p>Click the following button to see the result: </p>
+<form>
+<input type="button" value="Click Me" onclick="Warn();" />
+<input type="button" value="Pencet" onclick="Warn2();" />
+</form>
+</body>
+</html>
---
+++ alertdialog1.js
@@ -0,0 +1,2 @@
+function Warn() {alert ("This is a warning messege!"); document.write ('This is a warning msg!")}
+function Warn2() {alert ("This is another warning messege!"); document.write ('This is another warning msg!")}
